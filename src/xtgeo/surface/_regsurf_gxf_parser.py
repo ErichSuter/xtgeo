4ae6f6edf59e06d3a5e52c11c7301cf70b8ece84
Erich Suter <esut@equinor.com>
2026-04-16 18:42:03 +0200

    WIP: verything carried over from other versions

--- src/xtgeo/surface/_regsurf_gxf_parser.py
+++ src/xtgeo/surface/_regsurf_gxf_parser.py
@@ -6,6 +6,33 @@ only a subset of the keys are supported.
 
 Documentation: https://pubs.usgs.gov/of/1999/of99-514/grids/gxf.pdf
 
+
+
+The file consists of the following items:
+
+! comments starts with '!'
+    - Comment: ignored by the file reader.
+
+Free text
+    - Comment: any line that does NOT start with '!' or '#' or "##" is considered
+    free text and ignored by the file reader.
+
+#KEY
+value for KEY (on next line after KEY)
+    - Comment: a KEY preceeded with a single '#' means that the KEY is part
+    of the GFX specification
+    - Comment: the GFX format does not allow value in quotes, but some files
+    have them, they are stripped away if present.
+
+##KEY (double #)
+value for KEY (on next line after KEY)
+    - Comment: a KEY preceeded by '##' means that the KEY is NOT part
+    of the GFX specification, but added by some software or user.
+    We ignore such keys, but log a warning.
+
+
+
+
 Supported keys are:
 
 - #POINTS
@@ -42,22 +69,130 @@ the scope of this module.
 Nevertheless, a typical use is to let the #GRID data represent surface elevation.
 
 
-# TODO: check this, then delete comment
-#GRID contains values in row-major order (rows of length ncol),
-# but XTGeo's internal regular surface representation is (ncol, nrow),
-# therefore a transpose is applied on read/write.
-
-The #SENSE key controls both the orientation of the grid and
-right-handedness/left-handedness of the coordinate system, see the GFX documentation.
-The current implementation does not take this parameter into account.
-If the #SENSE key is present, a warning is issued to let the user know that
-the grid orientation and coordinate system handedness are handled in
-a default manner.
-
 Lines starting with ``!`` are comments and ignored. Lines that do not start with
 ``#`` are considered free text and ignored (outside the ``#GRID`` section).
 
 Keys prefixed with ``##`` are treated as extension keys and ignored with a warning.
+
+
+# TODO
+- Everything in Jan's prototype and branch "1338-gxf-regular-surface" is now
+carried over into this module
+- Ask CoPilot for differences between this version and branch "1338-gxf-regular-surface"
+- Create RegularSurface by importing from GXFSurface, following the normal xtgeo conventions.
+- Consider 'yflip' logic
+
+
+==================================================================================
+About some of the keys:
+==================================================================================
+
+# TODO: verify this
+#GRID: contains the grid values in row-major order (rows of length ncol),
+but XTGeo's internal regular surface representation is (ncol, nrow),
+therefore a transpose is applied on read/write.
+        # Notes:
+        #  - There should be a value for each grid point,
+        #       so nrow * ncol values in total.
+        #  - The values are expected to be in row-major order, i.e. all values for
+        #    the first row, then all values for the second row, etc.
+        #  - Values for a row can be distributed over multiple lines.
+
+
+
+#SENSE: controls both the orientation of the grid and
+right-handedness/left-handedness of the coordinate system.
+The current xtgeo implementation does not take this parameter into account.
+If the #SENSE key is present, a warning is issued to let the user know that
+the grid orientation and coordinate system handedness are handled in
+a default manner.
+
+# TODO: verify this
+#ROTATION: The GXF specification defines rotation as positive counterclockwise,
+but does not specify the reference direction for 0 degrees.
+The current xtgeo implementation assumes that 0 degrees means that the rows are aligned
+with the y-axis and columns with the x-axis, and that positive rotation
+is counterclockwise from this orientation.
+This should be verified with test files and users, and the documentation updated
+accordingly.
+                    elif key == "ROTATION":
+                        # Counter-clockwise angle in degrees of the
+                        # bottom edge of the grid with respect to
+                        # the Base Coordinate System X axis.
+                        rot = float(line[0].strip('"'))
+
+                        # TODO: rotation in GFX seems to be defined equally to
+                        # the rotation in RegularSurface
+                        # Hence the commented conversion should not be necessary
+                        args["rotation"] = rot
+                        # if rot != 0.0:
+                        #     args["rotation"] = -1 * (90 - rot)
+                        # else:
+                        #     args["rotation"] = rot
+
+
+
+# TODO: Jan left a comment in his prototype that he did no solve
+# some mysteries with origins.
+# Verify that the origins are correctly handled, and if not,
+# ask him what he meant
+
+
+        # TODO: probably need to make it right-handed and/or flip y-direction,
+        # depending on the SENSE parameter, which is currently not handled
+
+        # TODO: is the following necessary?
+        # values = np.array(values)
+        # args["rotation"] = 0.0
+        # revert columns to make it right handed
+        # values = np.flipud(values)
+
+        
+# TODO: ask Jan to verify this logic:
+        # It is assumed to be independent of the GFX SENSE parameter,
+        # which is not handled in the current implementation
+        args["yflip"] = 1
+        if args["yinc"] < 0.0:
+            args["yinc"] *= -1
+            args["yflip"] = -1
+
+
+
+==================================================================================
+Example of a valid GXF file:
+==================================================================================
+! This is a comment line, starting with '!'
+
+Some free text
+
+#POINTS
+"330"                     << number of columns
+#ROWS
+"208"                     << number of rows
+#PTSEPARATION
+"30.4800600"              << point separation in x direction (xinc)
+#RWSEPARATION
+"30.4800600"              << row separation in y direction (yinc)
+#XORIGIN
+"427391.726575"           << x origin (xori)
+#YORIGIN
+"7250373.922731"          << y origin (yori)
+#ROTATION
+"66.57993141719481"       << rotation in degrees (rotation)
+#DUMMY
+"9999999.0"               << value in undefined nodes
+##XMAX
+"439101.301489"           << x maximum (not part of GXF specification)
+##YMAX
+"7260149.299141"          << y maximum (not part of GXF specification)
+#GRID
+9999999.0      9999999.0     9999999.0         9999999.0         9999999.0
+9999999.0      9999999.0     9999999.0         9999999.0         9999999.0
+9999999.0      9999999.0     9999999.0         9999999.0         9999999.0
+3288.2225       2837.758      2844.5764      2850.4048       2828.643
+3289.9615      2830.3948      2813.4902      2812.1143      2810.7473
+3253.7393        2793.37      2790.6665      2789.2434      2789.8645
+... (more values in #GRID section, total of ncol*nrow values, in rows of length ncol)
 """
 
 from __future__ import annotations
@@ -85,7 +220,6 @@ logger = logging.getLogger(__name__)
 class GXFSurface:
     """Internal immutable data representation for a regular surface in GXF format."""
 
-    # GXF spec default values for optional keys.
     DEFAULTS: ClassVar[dict[str, float]] = {
         "PTSEPARATION": 1.0,
         "RWSEPARATION": 1.0,
@@ -360,7 +494,7 @@ class GXFSurface:
 
         Args:
             file: Path to GXF file or a file-like object (BytesIO or StringIO).
-            encoding: Text encoding for the input file (default: 'utf-8').
+            encoding: Text encoding for the input file.
         """
 
         wrapped_file = FileWrapper(file)
@@ -383,7 +517,7 @@ class GXFSurface:
 
         Args:
             file: Path to GXF file or a file-like object (BytesIO or StringIO).
-            encoding: Text encoding for the output file (default: 'utf-8').
+            encoding: Text encoding for the output file.
         """
 
         wrapped_file = FileWrapper(file)
